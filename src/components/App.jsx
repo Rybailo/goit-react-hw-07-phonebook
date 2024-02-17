898dbad77ff23112a91e6c7c7b9a069898009a0b
Rybailo <rybailo.orest4@gmail.com>
2024-02-17 22:41:04 +0200

    .

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,22 @@
 import { Toaster } from 'react-hot-toast';
 import { ContactForm } from '../components/ContactForm/ContactForm';
 import { ContactList } from '../components/ClontactList/ClontactList';
-import { Filter } from './Filter/Filter';
-import { Layout } from './Layout';
+import { Filter } from '../components/Filter/Filter';
+import { Layout } from '../components/Layout';
+import { store } from '../redux/store';
+import { Provider } from 'react-redux';
 
 export const App = () => {
   return (
-    <Layout>
-      <h1>Phonebook</h1>
-      <ContactForm />
-      <h2>Contacts</h2>
-      <Filter />
-      <ContactList />
-      <Toaster />
-    </Layout>
+    <Provider store={store}>
+      <Layout>
+        <h1>Phonebook</h1>
+        <ContactForm />
+        <h2>Contacts</h2>
+        <Filter />
+        <ContactList />
+        <Toaster />
+      </Layout>
+    </Provider>
   );
 };
